bfed34699c555c4f262a8ef0054a472fe3680737
scardona14 <cardanasean123@gmail.com>
2026-03-13 18:15:49 -0400

    feat: implement basic chat input with message sending functionality

--- app/page.tsx
+++ app/page.tsx
@@ -519,7 +519,7 @@ export default function TennisMatchOrganizer() {
                     <CardContent className="p-8 text-center">
                       <div className="mx-auto mb-4 flex h-16 w-16 items-center justify-center">
                         <Image
-                          src="/tennis-ball-realistic.png"
+                          src="/apple-icon.png"
                           alt="Tennis Ball"
                           width={64}
                           height={64}
